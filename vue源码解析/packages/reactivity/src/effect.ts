24b59219c9aaa97a8ea03aa9c6abe228298b0d89
chen <1710312281@QQ.COM>
2025-07-30 20:47:37 +0800

    730

--- vue源码解析/packages/reactivity/src/effect.ts
+++ vue源码解析/packages/reactivity/src/effect.ts
@@ -1,10 +1,15 @@
-export function effect(fn: Function, options: any) {
+export function effect(fn: Function, options: object) {
     //创建响应effect 数据变化后重新执行
     const _effect = new ReactiveEffect(fn, () => {
         _effect.run();
     });
     _effect.run()
-    return _effect;
+    if (options) {
+        Object.assign(_effect, options);
+    }
+    const runner: any = _effect.run.bind(_effect);
+    runner.effect = _effect
+    return runner;
 }
 
 export let activeEffect: ReactiveEffect | undefined = undefined;
@@ -13,6 +18,18 @@ function preCleanEffect(effect: ReactiveEffect) {
     //清理之前的依赖
     effect._depsLength = 0
     effect._trackId++ //每次执行effect时，trackId加1，表示新的依赖
+}
+
+function postCleanEffect(effect: ReactiveEffect) {
+    //清理依赖
+    if (effect.deps.length > effect._depsLength) {
+        //如果依赖长度大于当前依赖长度，说明有多余的依赖，需要清理
+        for (let i = effect._depsLength; i < effect.deps.length; i++) {
+            const dep = effect.deps[i];
+            cleanDepEffect(dep, effect); //清理依赖
+        }
+        effect.deps.length = effect._depsLength; //重置依赖长度
+    }
 }
 
 class ReactiveEffect {
@@ -36,8 +53,17 @@ class ReactiveEffect {
             preCleanEffect(this); //清理之前的依赖
             return this.fn();
         } finally {
+            postCleanEffect(this)
             activeEffect = lastEffect; //执行完后清除activeEffect
         }
+    }
+}
+
+function cleanDepEffect(dep: any, effect: ReactiveEffect) {
+    dep.delete(effect); //从依赖中删除当前effect
+    if (dep.size === 0) {
+        //如果依赖为空，清理依赖
+        dep.cleanup();
     }
 }
 
@@ -45,9 +71,17 @@ class ReactiveEffect {
 export function trackEffects(effect: ReactiveEffect, dep: Map<any, any>) {
     if (dep.get(effect) !== effect._trackId) {
         //依赖dep记录effect  同时effect记录依赖dep
-        dep.set(effect, effect._trackId); //将当前effect添加到依赖中
+        dep.set(effect, effect._trackId); //将当前effect添加到依赖中 更新id
+        let oldDep = effect.deps[effect._depsLength];
+        if (oldDep !== dep) {
+            if (oldDep) {
+                cleanDepEffect(oldDep, effect); //清理之前的依赖
+            }
+            effect.deps[effect._depsLength++] = dep;
+        } else {
+            effect._depsLength++
+        }
     }
-    effect.deps[effect._depsLength++] = dep; //将依赖添加到effect的deps中
 }
 
 export function trrigerEffects(dep: Map<any, any>) {
